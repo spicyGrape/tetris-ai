9f775a5edff3c5adee91525bc0b74bdf89feb60d
Shuaiting Li <li.private0@gmail.com>
2024-11-04 19:04:51 +0000

    Modified score rule to COMP0034 rule

--- dqn_agent.py
+++ dqn_agent.py
@@ -4,6 +4,7 @@ from collections import deque
 import numpy as np
 import random
 
+
 # Deep Q Learning Agent + Maximin
 #
 # This version only provides one value per input,
@@ -13,8 +14,7 @@ import random
 # in contrast to the traditional way of finding the best
 # action for a particular state.
 class DQNAgent:
-
-    '''Deep Q Learning Agent + Maximin
+    """Deep Q Learning Agent + Maximin
 
     Args:
         state_size (int): Size of the input domain
@@ -29,16 +29,29 @@ class DQNAgent:
         optimizer (obj): Optimizer used
         replay_start_size: Minimum size needed to train
         modelFile: Previously trained model file path to load (arguments such as activations will be ignored)
-    '''
+    """
 
-    def __init__(self, state_size, mem_size=10000, discount=0.95,
-                 epsilon=1, epsilon_min=0, epsilon_stop_episode=0,
-                 n_neurons=[32, 32], activations=['relu', 'relu', 'linear'],
-                 loss='mse', optimizer='adam', replay_start_size=None, modelFile=None):
+    def __init__(
+        self,
+        state_size,
+        mem_size=10000,
+        discount=0.95,
+        epsilon=1,
+        epsilon_min=0,
+        epsilon_stop_episode=0,
+        n_neurons=[32, 32],
+        activations=["relu", "relu", "linear"],
+        loss="mse",
+        optimizer="adam",
+        replay_start_size=None,
+        modelFile=None,
+    ):
 
         if len(activations) != len(n_neurons) + 1:
-            raise ValueError("n_neurons and activations do not match, "
-                             f"expected a n_neurons list of length {len(activations) - 1}")
+            raise ValueError(
+                "n_neurons and activations do not match, "
+                f"expected a n_neurons list of length {len(activations) - 1}"
+            )
 
         if replay_start_size is not None and replay_start_size > mem_size:
             raise ValueError("replay_start_size must be <= mem_size")
@@ -53,8 +66,10 @@ class DQNAgent:
         if epsilon_stop_episode > 0:
             self.epsilon = epsilon
             self.epsilon_min = epsilon_min
-            self.epsilon_decay = (self.epsilon - self.epsilon_min) / (epsilon_stop_episode)
-        else: # no random exploration
+            self.epsilon_decay = (self.epsilon - self.epsilon_min) / (
+                epsilon_stop_episode
+            )
+        else:  # no random exploration
             self.epsilon = 0
         self.n_neurons = n_neurons
         self.activations = activations
@@ -71,11 +86,16 @@ class DQNAgent:
         else:
             self.model = self._build_model()
 
-
     def _build_model(self):
-        '''Builds a Keras deep neural network model'''
+        """Builds a Keras deep neural network model"""
         model = Sequential()
-        model.add(Dense(self.n_neurons[0], input_dim=self.state_size, activation=self.activations[0]))
+        model.add(
+            Dense(
+                self.n_neurons[0],
+                input_dim=self.state_size,
+                activation=self.activations[0],
+            )
+        )
 
         for i in range(1, len(self.n_neurons)):
             model.add(Dense(self.n_neurons[i], activation=self.activations[i]))
@@ -83,36 +103,31 @@ class DQNAgent:
         model.add(Dense(1, activation=self.activations[-1]))
 
         model.compile(loss=self.loss, optimizer=self.optimizer)
-        
+
         return model
 
-
     def add_to_memory(self, current_state, next_state, reward, done):
-        '''Adds a play to the replay memory buffer'''
+        """Adds a play to the replay memory buffer"""
         self.memory.append((current_state, next_state, reward, done))
 
-
     def random_value(self):
-        '''Random score for a certain action'''
+        """Random score for a certain action"""
         return random.random()
 
-
     def predict_value(self, state):
-        '''Predicts the score for a certain state'''
+        """Predicts the score for a certain state"""
         return self.model.predict(state, verbose=0)[0]
 
-
     def act(self, state):
-        '''Returns the expected score of a certain state'''
+        """Returns the expected score of a certain state"""
         state = np.reshape(state, [1, self.state_size])
         if random.random() <= self.epsilon:
             return self.random_value()
         else:
             return self.predict_value(state)
 
-
     def best_state(self, states):
-        '''Returns the best state for a given collection of states'''
+        """Returns the best state for a given collection of states"""
         max_value = None
         best_state = None
 
@@ -128,14 +143,15 @@ class DQNAgent:
 
         return best_state
 
-
     def train(self, batch_size=32, epochs=3):
-        '''Trains the agent'''
+        """Trains the agent"""
         if batch_size > self.mem_size:
-            print('WARNING: batch size is bigger than mem_size. The agent will not be trained.')
+            print(
+                "WARNING: batch size is bigger than mem_size. The agent will not be trained."
+            )
 
         n = len(self.memory)
-    
+
         if n >= self.replay_start_size and n >= batch_size:
 
             batch = random.sample(self.memory, batch_size)
@@ -159,14 +175,19 @@ class DQNAgent:
                 y.append(new_q)
 
             # Fit the model to the given values
-            self.model.fit(np.array(x), np.array(y), batch_size=batch_size, epochs=epochs, verbose=0)
+            self.model.fit(
+                np.array(x),
+                np.array(y),
+                batch_size=batch_size,
+                epochs=epochs,
+                verbose=0,
+            )
 
             # Update the exploration variable
             if self.epsilon > self.epsilon_min:
                 self.epsilon -= self.epsilon_decay
 
-
     def save_model(self, name):
-        '''Saves the current model.
-        It is recommended to name the file with the ".keras" extension.'''
+        """Saves the current model.
+        It is recommended to name the file with the ".keras" extension."""
         self.model.save(name)
